ba86b31d7b1c3997d985e6221ecd3a1032db756a
Elam126 <41664953+Elam126@users.noreply.github.com>
2020-03-05 16:07:28 +0530

    phase1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, pipe } from 'rxjs';
+import {NavController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  constructor() {}
+  public items: Array<any> = [];
+
+  slideOpts = {
+    
+    speed: 400,
+    effect: 'coverflow',
+    slidesPerView: 1,
+    slideOffsetBefore: 10,
+    slideOffsetAftrer: 10,
+    direction: 'vertical',
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 20,
+      streach: 100,
+      depth: 100,
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      modifier: 1,
+    }
+  };
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.getData();
+    
+  }
+
+  getData(){
+    let url= 'https://madras-daily.herokuapp.com/api/news';
+    let data: Observable<any> = this.http.get(url);
+    data.subscribe(result =>{
+      this.items = result;
+      
+     // console.log(result);
+    });
+  }
+
 
 }
